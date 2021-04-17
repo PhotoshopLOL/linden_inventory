d26d42dbafcda263220f421487a99232c927e89a
joshphotoshop <joshphotoshop@gmail.com>
2021-04-17 14:18:25 +0800

    Updated to 1.2.8
    Signed-off-by: joshphotoshop <joshphotoshop@gmail.com>

--- client/main.lua
+++ client/main.lua
@@ -891,16 +891,6 @@ RegisterNUICallback('useItem', function(data, cb)
 	if data.inv == 'Playerinv' then TriggerEvent('linden_inventory:useItem', data.item) end
 end)
 
-RegisterNUICallback('giveItem', function(data, cb)
-	local closestPlayer, closestPlayerDistance = ESX.Game.GetClosestPlayer()
-	if closestPlayer == -1 or closestPlayerDistance > 2.0 then 
-		error('There is nobody nearby')
-	elseif data.inv == 'Playerinv' then
-		if data.amount >= 1 then
-			TriggerServerEvent('linden_inventory:giveItem', data, GetPlayerServerId(closestPlayer))
-		else error('You must enter an amount to give') end
-	end
-end)
 
 RegisterNUICallback('saveinventorydata',function(data)
 	TriggerServerEvent('linden_inventory:saveInventoryData', data)
